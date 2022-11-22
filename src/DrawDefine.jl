31c56b4102c66027831bd423db53262a066a250d
Cris Moen <cris.moen@runtosolve.com>
2022-11-22 16:03:42 -0500

    fix drawing scale definition

--- src/DrawDefine.jl
+++ src/DrawDefine.jl
@@ -1,6 +1,6 @@
 module DrawDefine
 
-using Luxor
+using Luxor, Rotations
 
 
 function create_drawing(;drawing_width, drawing_height, filename, background_color, font_name)
@@ -87,7 +87,7 @@ function add_section_title(;section_name, section_title_location, drawing_scale)
 	
 	fontface("Arial Regular") 
 	fontsize(10)
-	section_title = textbox(string("SCALE: ", floor(Int, drawing.scale/12), "\" = 1'-0\"", )  ,
+	section_title = textbox(string("SCALE: ", floor(Int, drawing_scale/12), "\" = 1'-0\"", )  ,
 	Luxor.Point(section_title[1], section_title[2] + textextents(section_name)[4] * 1.0),
 	leading = 6, alignment=:center)
 
